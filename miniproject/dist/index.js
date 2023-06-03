0abfa569d7fd38ad652a34504e369cc8342506e7
Uchirai <uchirai.govere@gmail.com>
2023-06-03 12:13:12 +0200

    basic app structure

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
-const btn = document.getElementById('btn');
-btn.addEventListener('click', function () {
-    alert('clicked!!!');
-});
-let mystery = 4;
-const numchars = mystery.length;
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoiput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    newLI.append(newTodoText);
+    list.append(newLI);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
+// let mystery: unknown = 4
+// const numchars = (mystery as string).length
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,5 +1,18 @@
-const btn = document.getElementById('btn')!
+const btn = document.getElementById("btn")! as HTMLButtonElement
+const input = document.getElementById("todoiput")! as HTMLInputElement
+const form = document.querySelector("form")! as HTMLFormElement
+const list = document.getElementById("todolist")! 
 
-btn.addEventListener('click', function() {
-    alert('clicked!!!')
-}) 
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    newLI.append(newTodoText);
+    list.append(newLI);
+    input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit)
+
+// let mystery: unknown = 4
+// const numchars = (mystery as string).length
