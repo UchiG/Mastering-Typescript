b9f94c104caa1ba6773ed81cbb32053b6c42ec37
Uchirai <uchirai.govere@gmail.com>
2023-06-04 18:56:00 +0200

    installing types for third party library lodash

--- thirdpartylibs/dist/index.js
+++ thirdpartylibs/dist/index.js
@@ -1,2 +1,47 @@
 "use strict";
-console;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+{
+    id: 1,
+        name;
+    'Leanne Graham',
+        username;
+    'Bret',
+        email;
+    'Sincere@april.biz',
+        address;
+    {
+        street: 'Kulas Light',
+            suite;
+        'Apt. 556',
+            city;
+        'Gwenborough',
+            zipcode;
+        '92998-3874',
+            geo;
+        {
+            lat: '-37.3159',
+                lng;
+            '81.1496';
+        }
+    }
+    phone: '1-770-736-8031 x56442',
+        website;
+    'hildegard.org',
+        company;
+    {
+        name: 'Romaguera-Crona', catchPhrase;
+        'Multi-layered client-server neural-net',
+            bs;
+        'harness real-time e-markets';
+    }
+}
+axios_1.default.get("https://jsonplaceholder.typicode.com/users/1")
+    .then((res) => {
+    console.log("PraiseGOD");
+    console.log(res.data);
+})
+    .catch((e) => console.log("Thank GOD"));
