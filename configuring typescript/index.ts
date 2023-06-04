0f9b95be8af8151c5a9644d9fb542301453bec0d
Uchirai <uchirai.govere@gmail.com>
2023-06-04 13:01:25 +0200

    messing with the type parameters

--- configuring typescript/index.ts
+++ configuring typescript/index.ts
@@ -1,13 +1,49 @@
-interface Chicken {
-    breed: string;
-    eggsPerWeek: number;
-    name: string;
-    age: number;
+function merge<T extends object, U extends object>(object1: T, object2: U) {
+    return {
+        ...object1,
+        ...object2,
+    }
 }
 
-const norma: Chicken = {
-    breed: "Silkie",
-    eggsPerWeek: 4,
-    name: "Norma",
-    age: 2,
+const comboObj = merge({ name: 'John' }, { ages: [30, 31, 32] })
+
+interface Lengthy {
+    length: number
 }
+
+function printDoubleLength<T extends Lengthy>(thing: T): number {
+    return thing.length * 2
+}
+
+console.log(printDoubleLength('Hello World'))
+
+
+function makeEmptyArray<T = number>(): T[] {
+    return []
+}
+
+const nums = makeEmptyArray() 
+const strings = makeEmptyArray<string>()
+
+interface Song {
+    title: string
+    artist: string
+}
+
+interface Video {
+    title: string
+    creator: string
+    resolution: string
+}
+
+class Playlist<T> {
+    public queue: T[] = []
+    add(el: T) {
+        this.queue.push(el)
+    }
+}
+
+const songs = new Playlist<Song> ()
+songs.add({title: 'Praises', artist: 'Hillsong United'})
+const videos = new Playlist<Video> ()
+videos.add({title: 'music video', creator: 'Hillsong', resolution: '1080p'})
